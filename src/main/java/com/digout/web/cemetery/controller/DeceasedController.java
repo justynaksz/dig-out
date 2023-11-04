9ca512cb184839bc9b5a2552feee751c0f5651c5
justynasz <justynaszczygiol@gmail.com>
2023-11-04 20:54:24 +0100

    1.3.2 ::: Refactor

--- src/main/java/com/digout/web/cemetery/controller/DeceasedController.java
+++ src/main/java/com/digout/web/cemetery/controller/DeceasedController.java
@@ -3,7 +3,13 @@ package com.digout.web.cemetery.controller;
 import com.digout.repository.cemetery.model.Deceased;
 import com.digout.repository.cemetery.repository.DeceasedRepository;
 import com.digout.repository.cemetery.repository.GraveRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
--- src/main/java/com/digout/web/cemetery/controller/LocalizationController.java
+++ src/main/java/com/digout/web/cemetery/controller/LocalizationController.java
@@ -2,7 +2,13 @@ package com.digout.web.cemetery.controller;
 
 import com.digout.repository.cemetery.model.Localization;
 import com.digout.repository.cemetery.repository.LocalizationRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
